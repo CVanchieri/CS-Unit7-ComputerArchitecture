c056fc3f3a47d2f8537b36d2f503aeab99fdfa2f
Charles Vanchieri <32582544+CVanchieri@users.noreply.github.com>
2020-10-07 15:48:26 -0700

    updated assignment 3, completed branch table too

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,46 +1,92 @@
 """CPU functionality."""
 import sys
+LDI     = 0b10000010
+PRN     = 0b01000111
+HLT     = 0b00000001
+MUL     = 0b10100010
+POP     = 0b01000110
+PUSH    = 0b01000101
 
 class CPU:
     """Main CPU class."""
-### Add list properties to the CPU class to hold 256 bytes of memory and 8 general-purpose registers. ###
+### Add list properties to the CPU class to hold 256 bytes of memory and 8 general-purpose registers ###
     def __init__(self):
         """Construct a new CPU."""
         self.reg = [0] * 8 
         self.ram = [0] * 256
         self.pc = 0
         self.halted = False
-        
-        self.instructions = { # dictionary with the necessary isntructions.
-                            'LDI' : 0b10000010,
-                            'PRN' : 0b01000111,
-                            'HLT' : 0b00000001,
-                            'MUL' : 0b10100010
-                            }
-
+        self.SP = 3 # start the SP at 3 
         self.address = 0
-### In CPU, add method ram_read() and ram_write() that access the RAM inside the CPU object. ###
-### ram_read() should accept the address to read and return the value stored there. ###
-    def ram_read(self, address):
-        print(self.ram[address])
-### ram_write() should accept a value to write, and the address to write it to. ###
-    def ram_write(self, address, instruction):
-        self.ram[address] = instruction
-        self.address += 1
+        """list or dictionary of functions that you can index by opcode value, fetch the instruction value from RAM, 
+        then use that value to look up the handler function in the branch table, then call it.""" 
+        self.bt = { # branch table
+            LDI: self.oper_ldi,
+            PRN: self.oper_prn,
+            HLT: self.oper_hlt,
+            MUL: self.oper_mul,
+            POP: self.oper_pop,
+            PUSH: self.oper_push,
+            }
 
-        ### Hardcoded a program ###
-       # program = [
-            # From print8.ls8
-           # 0b10000010, # LDI R0,8
-           # 0b00000000,
-           # 0b00001000,
-           # 0b01000111, # PRN R0
-            #0b00000000,
-           # 0b00000001, # HLT
-      #  ]
+    """branch table operations"""
+    ### LDI operation ###
+        # this instruction sets a specified register to a specified value
+    def oper_ldi(self, oper_a, oper_b):
+        self.reg[oper_a] = oper_b
+    
+    ### PRN operation ###
+    # very similar process to adding LDI, but the handler is simpler, See the LS-8 spec
+    def oper_prn(self, oper_a, oper_b):
+        print(self.reg[oper_a])
 
-    ### Add the load method ###
-    ### use the command line arguments to open a file, read in its contents line by line, and save appropriate data into RAM.
+    ### HLT operation ###
+    # exit the loop if a HLT instruction is encountered, regardless of whether or not there are more lines of code in the LS-8 program you loaded
+    def oper_hlt(self, oper_a, oper_b):
+        sys.exit(0)
+
+    ### MUL operation ###
+    # multiply the values in two registers together and store the result in register
+    def oper_mul(self, oper_a, oper_b):
+        self.alu("MUL", oper_a, oper_b)
+
+    ### POP operation ### 
+    # set reg[oper_a] with the pop_val method
+    def oper_pop(self, oper_a, oper_b):
+        self.reg[oper_a] = self.pop_val()
+
+    ### PUSH operation ###
+    # use push_val method on reg[oper_a]
+    def oper_push(self, oper_a, oper_b):
+        self.push_val(self.reg[oper_a])
+
+    """CPU methods"""
+    ### ram_read method ###
+    # ram_read() should accept the address to read and return the value stored there 
+    def ram_read(self, mar):
+        address = self.ram[mar]
+        return address
+
+    ### ram_write method ###
+    # ram_write() should accept a value to write, and the address to write it to
+    def ram_write(self, mdr, mar):
+        self.ram[mar] = mdr
+
+    ### push method ###
+    # decrement the SP, push the value stored in register to the address of the SP
+    def push_val(self, val):
+        self.reg[self.SP] -= 1
+        self.ram_write(val, self.reg[self.SP])
+
+    ### pop method ###
+    # pop the value at the top of the stack into the given register, Copy the value, increment SP
+    def pop_val(self):
+        val = self.ram_read(self.reg[self.SP])
+        self.reg[self.SP] += 1
+        return val
+
+    ### load method ###
+    # use the command line arguments to open a file, read in its contents line by line, and save appropriate data into RAM
     def load(self):
         """Load a program into memory."""
         program_name = sys.argv[1]
@@ -60,7 +106,6 @@ class CPU:
     ### alu method ###
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
@@ -87,78 +132,19 @@ class CPU:
         print()
 
     ### run method ###
-    """ *** need to work into branch tsble *** """
     def run(self):
         """Run the CPU."""
-        print('--- Analyzing Results ---')
-        while not self.halted:
-            instruction = self.ram[self.pc]
-            ### Add the LDI instruction ###
-            ### This instruction sets a specified register to a specified value. 
-            if instruction == self.instructions['LDI']: # equal to LDI
-                value = self.ram[self.pc + 2]
-                register_num = self.ram[self.pc + 1]
-                self.reg[register_num] = value 
-                self.pc += 3
-            ### Add the PRN instruction ###
-            ### Very similar process to adding LDI, but the handler is simpler. See the LS-8 spec. 
-            if instruction == self.instructions['PRN']: # equal to PRN
-                register_num = self.ram[self.pc + 1]
-                print(self.reg[register_num])
-                self.pc += 2
-            ### Add the HLT instruction define to cpu.h. ###
-            ### exit the loop if a HLT instruction is encountered, regardless of whether or not there are more lines of code in the LS-8 program you loaded. 
-            if instruction == self.instructions['HLT']: # equal to HLT
-                self.halted = True 
-                self.pc += 1
-            ### Add the MUL instruction, this is an instruction handled by the ALU ###
-            ### Multiply the values in two registers together and store the result in registerA. 
-            if instruction == self.instructions['MUL']:
-                reg_num1 = self.reg[self.ram[self.pc + 1]]
-                reg_num2 = self.reg[self.ram[self.pc + 2]]
-                self.reg[self.ram[self.pc + 1]] = reg_num1 * reg_num2
-                self.pc += 3
+        while not self.halted: # loop while halted is not False 
+            instruction = self.ram[self.pc] # set instruction to the ram[self.pc]
+            oper_a = self.ram_read(self.pc + 1) # set the oper_a to the ram_read(self.pc + 1)
+            oper_b = self.ram_read(self.pc + 2) # set the oper_b to the ram_read(self.pc + 2)
+            instruct_length = (instruction >> 6) + 1 # set the instruct_length to instruction shift R 6 + 1 
+            instruct_set_pc = ((instruction >> 4) & 0b1) == 1 # set the instruct_set_pc to instruction shift R 4 and 0b1 equal to 1 
 
-'''
-class Stack: # stack class
-    def __init__(self): # initializer constructor method
-        self.size = 0 # set size to 0
-        self.items = [] # set storage items to empty list
-
-    def __len__(self): # method to show the length
-        return self.size
-
-    def push(self, value): # method to add a value
-        self.items.append(value) # append the value to items list
-        self.size += 1 # add 1 to the length
-
-    def pop(self): # method to remove a value
-        if self.items != []: # if items list is not empty
-            self.size -= 1 # remove 1 from the length
-            return self.items.pop()
-
-class Stack:
-    #Constructor
-    def __init__(self):
-        self.stack = list()
-        self.maxSize = 8
-        self.top = 0
-    #Adds element to the Stack
-    def push(self,data):
-        if self.top>=self.maxSize:
-            return ("Stack Full!")
-        self.stack.append(data)
-        self.top += 1
-        return True
-    #Removes element from the stack
-    def pop(self):
-        if self.top<=0:
-            return ("Stack Empty!")
-        item = self.stack.pop()
-        self.top -= 1
-        return item
+            if instruction in self.bt: # if the instruction is in the branch table
+                self.bt[instruction](oper_a, oper_b) # run the instruction with operations
        
-    #Size of the stack
-    def size(self):
-        return self.top
-'''
+            if instruct_set_pc != 1: # if the value is not equal to 1
+                self.pc += instruct_length # increment pc by instruct_length
+
+    
